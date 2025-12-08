c3047e40c8f6a4742cb5b856419d95c19d9b0e73
TheMostEpicest <scottccs.frizzell@gmail.com>
2025-12-07 18:32:46 -0600

    Basic sidebar setup

--- client/map/index.js
+++ client/map/index.js
@@ -40,10 +40,89 @@ const kingdoms = [
 ];
 
 const kingdomList = document.getElementById("kingdom-list");
+const sidebar = document.getElementById("sidebar");
+const sidebarContent = document.getElementById("sidebar-content");
+const tabCaptures = document.getElementById("sidebar-tab-captures");
+const tabAbilities = document.getElementById("sidebar-tab-abilities");
+const tabSubAreas = document.getElementById("sidebar-tab-subAreas");
+const tabLoadingZones = document.getElementById("sidebar-tab-loadingZones");
+const tabAvailableChecks = document.getElementById("sidebar-tab-availableChecks");
+
+let currentKingdom = "sand";
+let currentSidebarTab = "captures";
+
+tabCaptures.onclick = updateSidebarTab;
+tabAbilities.onclick = updateSidebarTab;
+tabSubAreas.onclick = updateSidebarTab;
+tabLoadingZones.onclick = updateSidebarTab;
+tabAvailableChecks.onclick = updateSidebarTab;
+
+
 
 kingdoms.forEach((kingdom) => {
     let newDiv = document.createElement("div");
     newDiv.id = `kingdom-list-${kingdom}`;
-    newDiv.innerHTML = `<img src="/resource/kingdoms/${kingdom}.png" alt="${kingdom}" title="${kingdom}">`;
+    newDiv.innerHTML = `<img src="/resource/kingdoms/${kingdom}.png" alt="${kingdom}" title="${kingdom}" draggable="false">`;
+    newDiv.onclick = updateCurrentKingdom;
     kingdomList.appendChild(newDiv);
 });
+
+function updateCurrentKingdom(event) {
+    let target = event.target.tagName == "IMG" ? event.target.parentElement : event.target;
+
+    let newKingdom = target.id.split("-")[2];
+
+    document.getElementById(`kingdom-list-${currentKingdom}`).classList.remove("selected");
+    document.getElementById(`kingdom-list-${newKingdom}`).classList.add("selected");
+
+    currentKingdom = newKingdom;
+}
+
+function updateSidebarTab(event) {
+    let target = event.target.tagName == "P" ? event.target.parentElement : event.target;
+
+    let newSidebarTab = target.id.split("-")[2];
+
+    document.getElementById(`sidebar-tab-${currentSidebarTab}`).classList.remove("selected");
+    document.getElementById(`sidebar-tab-${newSidebarTab}`).classList.add("selected");
+
+    currentSidebarTab = newSidebarTab;
+
+    switch(newSidebarTab) {
+        case "captures":
+            setSidebarContentCaptures();
+            return;
+        case "abilities":
+            setSidebarContentAbilities();
+            return;
+        case "subAreas":
+            setSidebarContentSubAreas();
+            return;
+        case "loadingZones":
+            setSidebarContentLoadingZones();
+            return;
+        case "availableChecks":
+            setSidebarContentAvailableChecks();
+            return;
+    }
+}
+
+function setSidebarContentCaptures() {
+
+}
+
+function setSidebarContentAbilities() {
+
+}
+
+function setSidebarContentSubAreas() {
+
+}
+
+function setSidebarContentLoadingZones() {
+
+}
+
+function setSidebarContentAvailableChecks() {
+
+}
